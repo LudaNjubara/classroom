70f1b9cb9755a747a56fe45a4515766fd0ea1b47
LudaNjubara <mihael.sestak00@gmail.com>
2024-03-09 22:54:13 +0100

    Add Skeleton component to TeacherCardSkeleton

--- components/pages/dashboard/content/teacher-tab/TeacherCardSkeleton.tsx
+++ components/pages/dashboard/content/teacher-tab/TeacherCardSkeleton.tsx
@@ -1,13 +1,15 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function TeacherCardSkeleton() {
   return (
     <div className="flex gap-3 rounded-lg bg-slate-400 dark:bg-slate-900 overflow-hidden animate-pop-up">
       <div className="relative basis-20 flex-shrink-0">
-        <div className="w-20 h-20 bg-slate-500 dark:bg-slate-800 animate-pulse"></div>
+        <Skeleton className="w-20 h-20" />
       </div>
 
       <div className="flex-1 py-4 px-2">
-        <div className="w-24 h-4 bg-slate-500 dark:bg-slate-800 animate-pulse"></div>
-        <div className="w-32 h-4 bg-slate-500 dark:bg-slate-800 animate-pulse mt-1"></div>
+        <Skeleton className="w-24 h-4"></Skeleton>
+        <Skeleton className="w-32 h-4 mt-2"></Skeleton>
       </div>
     </div>
   );
