e022135ba07075fe6a8f61850617b4b0255410b7
LudaNjubara <mihael.sestak00@gmail.com>
2024-03-19 20:54:26 +0100

    Add dynamic styling to navigation buttons

--- components/Header/HeaderNavigation.tsx
+++ components/Header/HeaderNavigation.tsx
@@ -5,10 +5,13 @@ import { Button } from "@/components/ui/button";
 import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "@/components/ui/navigation-menu";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { KindeButton } from "../Elements";
 
 export function HeaderNavigation() {
   const { isAuthenticated, isLoading } = useKindeBrowserClient();
+  const pathname = usePathname();
 
   if (isLoading)
     return (
@@ -29,22 +32,31 @@ export function HeaderNavigation() {
         <NavigationMenuItem>
           <Button
             variant="ghost"
-            className="h-11 hover:bg-slate-100 transition-colors duration-200 dark:hover:bg-slate-900"
+            className={`h-11 transition-colors duration-200 ${
+              pathname === "/"
+                ? "bg-slate-100 dark:bg-slate-900"
+                : "hover:bg-slate-100 dark:hover:bg-slate-900"
+            }`}
           >
-            Item One
+            Home
           </Button>
         </NavigationMenuItem>
 
         <NavigationMenuItem>
           <Button
+            asChild
             variant="ghost"
-            className="h-11 hover:bg-slate-100 transition-colors duration-200 dark:hover:bg-slate-900"
+            className={`h-11 transition-colors duration-200 ${
+              pathname === "/dashboard"
+                ? "bg-slate-100 dark:bg-slate-900"
+                : "hover:bg-slate-100 dark:hover:bg-slate-900"
+            }`}
           >
-            Item Two
+            <Link href="/dashboard">Dashboard</Link>
           </Button>
         </NavigationMenuItem>
 
-        <NavigationMenuItem>
+        <NavigationMenuItem className="pl-5">
           <KindeButton type="logout" />
         </NavigationMenuItem>
       </NavigationMenuList>
