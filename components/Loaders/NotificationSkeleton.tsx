a4c1fef1ec3cc069bc26909aa84c9b79bdafb57a
LudaNjubara <mihael.sestak00@gmail.com>
2024-03-27 23:26:10 +0100

    Removed commented code

--- components/Loaders/NotificationSkeleton.tsx
+++ components/Loaders/NotificationSkeleton.tsx
@@ -1,28 +1,3 @@
-// create skeleton from this jsx and use Skeleton component from "@/components/ui/skeleton".
-/* 
-
-<div
-                  key={notification.id}
-                  className="p-4 bg-slate-500/30 dark:bg-slate-600/30 rounded-lg my-2"
-                >
-                  <div className="flex justify-between">
-                    <div>
-                      <h3 className="text-lg font-medium">{notification.recipientId}</h3>
-                      <p className="text-slate-600 dark:text-slate-600">{notification.message}</p>
-                    </div>
-
-                    <div className="flex items-center gap-4">
-                      <p className="text-slate-600 dark:text-slate-600">
-                        {notification.createdAt.toLocaleString()}
-                      </p>
-                      <Button variant="outline" size="sm">
-                        Mark as read
-                      </Button>
-                    </div>
-                  </div>
-                </div>
-*/
-
 import { Skeleton } from "@/components/ui/skeleton";
 
 export function NotificationSkeleton() {
