eabe61c4915c402deff4f2f316d5502c9ef1b192
LudaNjubara <mihael.sestak00@gmail.com>
2024-04-19 22:04:50 +0200

    Restructured the skeleton so it matches the actual notification style

--- components/Loaders/NotificationSkeleton.tsx
+++ components/Loaders/NotificationSkeleton.tsx
@@ -3,16 +3,21 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function NotificationSkeleton() {
   return (
     <div className="p-4 bg-slate-500/30 dark:bg-slate-600/30 rounded-lg my-2 animate-pop-up">
-      <div className="flex justify-between">
-        <div>
-          <Skeleton className="w-1/2 h-8"></Skeleton>
+      <div className="flex gap-4">
+        <div className="flex-1">
+          <div className="flex flex-col gap-4">
+            <Skeleton className="w-32 h-3"></Skeleton>
 
-          <Skeleton className="w-3/4 h-4 mt-2"></Skeleton>
+            <div className="flex flex-col gap-2">
+              <Skeleton className="w-52 h-4"></Skeleton>
+              <Skeleton className="w-[70%] h-8"></Skeleton>
+            </div>
+          </div>
         </div>
 
         <div className="flex items-center gap-4">
-          <Skeleton className="w-1/4 h-8"></Skeleton>
-          <Skeleton className="w-24 h-8"></Skeleton>
+          <Skeleton className="w-20 h-8"></Skeleton>
+          <Skeleton className="w-20 h-8"></Skeleton>
         </div>
       </div>
     </div>
