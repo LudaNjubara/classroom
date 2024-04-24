aade07825d2acec8d9d20575fff6c3f095427110
LudaNjubara <mihael.sestak00@gmail.com>
2024-04-24 21:39:02 +0200

    Added student invite notification type to the list of types which can perform actions

--- features/notifications/constants/notification-constants.ts
+++ features/notifications/constants/notification-constants.ts
@@ -1,3 +1,3 @@
 import { NotificationType } from "@prisma/client";
 
-export const NOTIFICATIONS_WITH_ACTIONS: NotificationType[] = ["ORGANIZATION_INVITE_TEACHER"];
+export const NOTIFICATIONS_WITH_ACTIONS: NotificationType[] = ["ORGANIZATION_INVITE_TEACHER", "ORGANIZATION_INVITE_STUDENT"];
