716ff870ac4efe1cfc9d83b9e336e5266db8d929
LudaNjubara <mihael.sestak00@gmail.com>
2024-04-24 21:46:32 +0200

    Change theming

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFoundPage() {
   return (
-    <div className="fixed inset-0 flex flex-col items-center justify-center w-full h-full p-4 bg-gradient-radial from-slate-50 to-slate-400 dark:from-slate-800 dark:to-slate-900">
+    <div className="fixed inset-0 flex flex-col items-center justify-center w-full h-full p-4 bg-gradient-radial from-slate-50 to-slate-400 dark:from-slate-900 dark:to-slate-950">
       <h1 className="text-4xl font-bold text-center text-slate-700 dark:text-slate-300">
         Oops, we don&apos;t know about this one
       </h1>
@@ -10,7 +10,7 @@ export default function NotFoundPage() {
         The page you&apos;re looking for doesn&apos;t exist. But hey, you can always go back{" "}
         <Link
           href="/"
-          className="py-1 px-2 rounded-sm bg-slate-300 dark:bg-slate-800 text-slate-500 dark:text-slate-400 hover:bg-slate-400 hover:text-slate-200 dark:hover:bg-slate-900 transition-colors duration-300"
+          className="py-1 px-2 rounded-sm bg-slate-300 dark:bg-slate-900 text-slate-500 dark:text-slate-400 hover:bg-slate-400 hover:text-slate-200 dark:hover:bg-slate-800 transition-colors duration-300"
         >
           Home
         </Link>
