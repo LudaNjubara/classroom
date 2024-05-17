8dc11b4f3285c46954a7a752b3ef59485fe9ae65
LudaNjubara <mihael.sestak00@gmail.com>
2024-05-17 17:08:59 +0200

    Removed unnecesary code

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
