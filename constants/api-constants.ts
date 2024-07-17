ee36232813b5e500ba6a645a2ecb1d535c4b3962
LudaNjubara <mihael.sestak00@gmail.com>
2024-07-18 00:02:53 +0200

    Add HTTP_STATUS_CODES constant

--- constants/api-constants.ts
+++ constants/api-constants.ts
@@ -77,3 +77,16 @@ export const API_ENDPOINTS = {
     PROFILE: `${BASE_API_ENDPOINT}/profile`,
     AUTH: `${BASE_API_ENDPOINT}/auth`,
 } as const
+
+export const HTTP_STATUS_CODES = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+    UNPROCESSABLE_CONTENT: 422,
+    INTERNAL_SERVER_ERROR: 500,
+} as const
