1ae174a7437a39654b3ac59c95f0bc906e5aee2f
LudaNjubara <mihael.sestak00@gmail.com>
2024-08-11 14:28:02 +0200

    Minor UI improvement

--- features/community/components/view-article/ArticleAuthor.tsx
+++ features/community/components/view-article/ArticleAuthor.tsx
@@ -7,7 +7,7 @@ export function ArticleAuthor({ author }: TArticleAuthorProps) {
     <div className="flex items-start gap-4">
       <img src={author.profile.picture} alt={author.name} className="w-10 h-10 rounded-full" />
 
-      <div>
+      <div className="-mt-1">
         <p className="text-lg font-semibold">{author.name}</p>
         <p className="text-sm text-muted-foreground">{author.profile.role}</p>
       </div>
